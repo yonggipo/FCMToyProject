c96770ed584d8194d5f64f2b3ea8eb83f8b160bd
yonggipo <yonggipo@icloud.com>
2024-07-05 14:31:08 +0900

    feat: add user notification outlet and function

--- FCMToyProject/ViewController.swift
+++ FCMToyProject/ViewController.swift
@@ -15,11 +15,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tokenTextView: UITextView!
     @IBOutlet weak var userInfoTextView: UITextView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextfield: UITextField!
+    @IBOutlet weak var sendUserNotificationButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHideNotification(_:)), name: Notification.Name.fcmToken, object: nil)
+        sendUserNotificationButton.addTarget(self, action: #selector(sendNotiButtonTapped(_:)), for: .touchUpInside)
     }
     
     deinit {
@@ -33,6 +37,32 @@ class ViewController: UIViewController {
             
             if let token = userInfo["token"] as? String {
                 tokenTextView.text = token
+            }
+        }
+    }
+    
+    @objc 
+    func sendNotiButtonTapped(_ sender: UIButton) {
+        let title = titleTextField.text ?? "title"
+        let body = bodyTextfield.text ?? "body"
+        presentNotificationAfter(seconds: 1.0, title: title, body: body)
+    }
+    
+    func presentNotificationAfter(seconds: TimeInterval, title: String, body: String) {
+        let notiContent = UNMutableNotificationContent()
+        notiContent.title = title
+        notiContent.body = body
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: seconds, repeats: false)
+        let request = UNNotificationRequest(
+            identifier: "testNotification",
+            content: notiContent,
+            trigger: trigger
+        )
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error {
+                Swift.print("Notification Error: ", error)
             }
         }
     }
