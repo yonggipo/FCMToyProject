aca9b63bbd035407712991532a050a349917213f
yonggipo <yonggipo@icloud.com>
2024-07-05 13:51:25 +0900

    feat: add notification and configure outlet

--- FCMToyProject/AppDelegate.swift
+++ FCMToyProject/AppDelegate.swift
@@ -26,6 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         )
         
         application.registerForRemoteNotifications()
+            
         return true
     }
 
@@ -46,6 +47,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
     }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async -> UIBackgroundFetchResult {
+        
+        return UIBackgroundFetchResult.newData
+    }
+    
+//    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+//        return true
+//    }
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
@@ -58,24 +68,17 @@ extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
-        // let userInfo = response.notification.request.content.userInfo
-    }
-    
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async -> UIBackgroundFetchResult {
-        // Handle data of notification
-        
-        return UIBackgroundFetchResult.newData
+        _ = response.notification.request.content.userInfo
     }
     
     // MARK: - MessagingDelegate
     
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
         guard let fcmToken else { return }
-        Swift.print("Fetched FCM token: \(fcmToken)")
         
         let dataDict: [String: String] = ["token": fcmToken]
         NotificationCenter.default.post(
-            name: Notification.Name("FCMToken"),
+            name: Notification.Name.fcmToken,
             object: nil,
             userInfo: dataDict
         )
--- FCMToyProject/ViewController.swift
+++ FCMToyProject/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension Notification.Name {
+    static let fcmToken = Notification.Name("FCMToken")
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tokenTextView: UITextView!
+    @IBOutlet weak var userInfoTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHideNotification(_:)), name: Notification.Name.fcmToken, object: nil)
     }
-
-
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc
+    func keyboardWillHideNotification(_ notification: Notification) {
+        if let userInfo = notification.userInfo {
+            userInfoTextView.text = userInfo.description
+            
+            if let token = userInfo["token"] as? String {
+                tokenTextView.text = token
+            }
+        }
+    }
 }
 
